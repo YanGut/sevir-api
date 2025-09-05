ea1b71051db641215360a5a2383de775c8157280
Yan Gutberg <guttlauncher@gmail.com>
2025-09-05 17:43:52 -0300

    :bug: fix: update database service name and improve configuration handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { ProductsModule } from './products/products.module';
 import { PricesModule } from './prices/prices.module';
 import { DatabaseModule } from './database/database.module';
 
+import config from './common/config/config';
+import { LoggingMiddleware } from './common/middlewares/logging.middleware';
+
 @Module({
-  imports: [DatabaseModule, ProductsModule, PricesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [config],
+    }),
+    DatabaseModule,
+    ProductsModule,
+    PricesModule,
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,17 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'pricewatch_user',
-      password: 'pricewatch_password',
-      database: 'pricewatch_db',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('database.postgres.host', 'localhost'),
+        port: configService.get<number>('database.postgres.port', 5433),
+        username: configService.get<string>('database.postgres.username', 'info_market_user'),
+        password: configService.get<string>('database.postgres.password', 'info_market_password'),
+        database: configService.get<string>('database.postgres.database', 'info_market_db'),
+        synchronize: configService.get<boolean>('database.synchronize', true),
+        dropSchema: configService.get<string>('environment') !== 'production',
+        autoLoadEntities: true,
+        logging: ['error'],
+      }),
     }),
   ],
   exports: [TypeOrmModule],
