5dc5cb6d5b327cca8f7f7f17beef985a216b5a2e
YanGut <guttlauncher@gmail.com>
2025-10-23 15:27:19 -0300

    :bug: fix: add TypeOrmModule import for VolunteerStatus entity in VolunteerStatusModule

--- src/modules/volunteer-status/volunteer-status.module.ts
+++ src/modules/volunteer-status/volunteer-status.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { VolunteerStatusService } from './volunteer-status.service';
 import { VolunteerStatusController } from './volunteer-status.controller';
+import { VolunteerStatus } from './entities/volunteer-status.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([VolunteerStatus])],
   controllers: [VolunteerStatusController],
   providers: [VolunteerStatusService],
 })
