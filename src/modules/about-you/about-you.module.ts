5f32f925153ccb18573ac15d8276d931abef6639
YanGut <guttlauncher@gmail.com>
2025-10-26 16:27:19 -0300

    :bug: fix: export AboutYouService from AboutYouModule for external usage

--- src/modules/about-you/about-you.module.ts
+++ src/modules/about-you/about-you.module.ts
@@ -8,5 +8,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [TypeOrmModule.forFeature([AboutYou])],
   controllers: [AboutYouController],
   providers: [AboutYouService],
+  exports: [AboutYouService],
 })
 export class AboutYouModule {}
